f6cdec8c820c7b1c3162c97613b10be269f1d14e
Numos <twitter: @numosX>
2021-12-12 03:18:38 +0900

    minor modification

--- libs/directional_control_helper/directional_control_helper.lua
+++ libs/directional_control_helper/directional_control_helper.lua
@@ -1,6 +1,6 @@
 -- DirectionalControlHelper
 -- Copyright (c) 2021 ぬーもす 
--- Twitter: @numosX 
+-- This software is released under the MIT License.
 
 local is_on = function(self)
     return self._is_on
